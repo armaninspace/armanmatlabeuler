4b1f8a5dd7df9c13dcaac9b1f0c2998a8a5c1202
aaanwar <arman.anwar2@gmail.com>
2013-01-18 23:27:45 -0500

    p4

--- problem3.m
+++ problem3.m
@@ -1,44 +1,34 @@
-function [ ] = problem3( )
+function [maxfactor] = problem3( )
 % The prime factors of 13195 are 5, 7, 13 and 29.
 % What is the largest prime factor of the number 600851475143 ?
 
-% 2 3 5 7 
-
-
-
-% Input: an integer n > 1
- 
-% Let A be an array of Boolean values, indexed by integers 2 to n,
-% initially all set to true.
- 
-% for i = 2, 3, 4, ..., ?n :
-%   if A[i] is true:
-%     for j = i2, i2+i, i2+2i, ..., n:
-%       A[j] := false
- 
-% Now all i such that A[i] is true are prime.
-
-    %upperlimit=ceil(sqrt (600851475143));
-    upperlimit=ceil(sqrt (10));
-    A=logical(1:upperlimit);
-    
-    
-    %A=A*0;
-    for i=2:upperlimit
-        disp('xxx');
-        if A(i)== true
-            n=0;
-            while true 
-                i_=i*i + n*i
-                if i_ > n break
-                end;
-                A(n)=false;
-               
+    num=600851475143;
+    upper=ceil(sqrt (num));
+    primes=getPrimes(upper);
+    maxfactor=0;
+    for i=1:upper
+        if primes(i) && (mod(num,i)==0)
+            maxfactor=i;
+        end
+    end
+   
+    function [primes] = getPrimes(n) 
+        upperlimit=floor(sqrt (n));
+        primes=logical(1:n);
+        primes(1)=false;
+        for i=2:upperlimit
+            if primes(i)== true
+                jj=0;
+                while true 
+                    j=i*i + jj*i;
+                    if j > n 
+                        break
+                    end;
+                    primes(j)=false;
+                    jj=jj+1;
+                end
             end
         end
     end
-    
-    A
-
 end
 
